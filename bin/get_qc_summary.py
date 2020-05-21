d0b9cb04c0fee946a3f2ae5af82d45c4cdd8c3e2
Richard de Borja <richard.deborja@gmail.com>
2020-05-21 08:59:31 -0400

    updated code to include additional fields for percent n bases, ct values, and separate indel and snv counts

--- bin/get_qc_summary.py
+++ bin/get_qc_summary.py
@@ -7,7 +7,7 @@ A Python package for summarizing QC data from the ncov-tools pipeline.
 import argparse
 import sys
 from ncov.parser.qc import create_qc_summary_line, write_qc_summary, \
-write_qc_summary_header
+write_qc_summary_header, import_ct_data
 
 
 parser = argparse.ArgumentParser(description="Tool for summarizing QC data")
@@ -18,6 +18,8 @@ parser.add_argument('-e', '--coverage', \
         help='<sample>.per_base_coverage.bed file to process')
 parser.add_argument('-i', '--indel', action='store_true', \
         help='flag to determine whether to count indels')
+parser.add_argument('-m', '--meta', \
+        help='full path to the metadata YAML file')
 
 if len(sys.argv) == 1:
     parser.print_help(sys.stderr)
@@ -25,10 +27,12 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
+ct_data = import_ct_data(file=args.meta)
 qc_line = create_qc_summary_line(
         var_file=args.variants,
         qc_file=args.qc,
         cov_file=args.coverage,
+        meta_file=args.meta,
         indel=args.indel)
 
 write_qc_summary_header()
--- ncov/parser/qc.py
+++ ncov/parser/qc.py
@@ -27,7 +27,7 @@ def get_qc_data(file):
             line = line.strip()
             data = line.split(",")
     file_p.close()
-    return {"sample_name" : data[0], "pct_covered_bases" : data[2], "qc_pass" : data[6]}
+    return {'sample_name' : data[0], 'pct_n_bases' : data[1], 'pct_covered_bases' : data[2], 'qc_pass' : data[6]}
 
 
 def get_total_variants(file, indel=False):
@@ -48,6 +48,8 @@ def get_total_variants(file, indel=False):
     counter = 0
     counter_n = 0
     counter_iupac = 0
+    counter_snv = 0
+    counter_indel = 0
     with open(file) as file_p:
         for line in file_p:
             if re.match("^REGION\tPOS\tREF", line):
@@ -57,11 +59,17 @@ def get_total_variants(file, indel=False):
             # indels
             data = line.split("\t")
             if indel:
-                if len(str(data[3])) > 1 or len(str(data[3])) == 1:
+                if len(str(data[3])) > 1:
                     counter += 1
+                    counter_indel += 1
+                elif len(str(data[3])) == 1:
+                    counter += 1
+                    counter_snv += 1
+
             elif not indel:
                 if len(str(data[3])) == 1:
                     counter += 1
+                    counter_snv += 1
                 else:
                     continue
             # count N and IUPAC codes seperately
@@ -70,7 +78,35 @@ def get_total_variants(file, indel=False):
             elif is_variant_iupac(variant=str(data[3])):
                 counter_iupac += 1
     file_p.close()
-    return {'total_variants' : counter, 'total_n' : counter_n, 'total_iupac' : counter_iupac}
+    return {'total_variants' : counter,
+            'total_n' : counter_n,
+            'total_iupac' : counter_iupac,
+            'total_snv' : counter_snv,
+            'total_indel' : counter_indel}
+
+
+def import_ct_data(file):
+    '''
+    Obtain the name of the metadata YAML file and import the data.
+
+    Arguments:
+        * file: full path to the metadata YAML file
+
+    Return Value:
+        The function returns a dictionary with {"sample" : "ct"}
+    '''
+    with open(file) as file_p:
+        tmp_data = []
+        data = {}
+        for line in file_p:
+            # skip the header if exists
+            if re.match("^sample\tct", line.lower()):
+                continue
+            line = line.strip()
+            tmp_data = line.split("\t")
+            data[tmp_data[0]] = tmp_data[1]
+        file_p.close()
+        return data
 
 
 def is_variant_n(variant):
@@ -146,32 +182,43 @@ def get_coverage_stats(file):
     return {"mean" : mean_depth, "median" : median_depth}
 
 
-def create_qc_summary_line(var_file, qc_file, cov_file, indel=True):
+def create_qc_summary_line(var_file, qc_file, cov_file, meta_file, indel=True):
     '''
     A function that aggregates the different QC data into a single sample
     dictionary entry.
 
     Arguments:
-        * var_file: full path to the <sample>.variants.tsv file
-        * qc_file: full path to the <sample>.qc.csv file
-        * cov_file: full path to the <sample>.per_base_coverage.bed file
-        * indel: boolean to determine whether to use indels in variant count
+        * var_file:     full path to the <sample>.variants.tsv file
+        * qc_file:      full path to the <sample>.qc.csv file
+        * cov_file:     full path to the <sample>.per_base_coverage.bed file
+        * meta_file:    full path to the 'metadata.tsv' file
+        * indel:        boolean to determine whether to use indels in variant
+                        count (default: True)
 
     Return Value:
-        Return an aggregate dictionary containing:
+        Return an aggregate dictionary containing the following keys:
             * total_variants
+            * total_snv
+            * total_indel
             * total_n
             * total_iupac
             * sample_name
+            * pct_n_bases
             * pct_covered_bases
             * qc_pass
             * mean
             * median
+            * ct
     '''
     summary = {}
     summary.update(get_total_variants(file=var_file, indel=indel))
     summary.update(get_qc_data(file=qc_file))
     summary.update(get_coverage_stats(file=cov_file))
+
+    # import the ct data from the metadata file and add the ct value to the summary dictionary
+    meta_data = import_ct_data(file=meta_file)
+    summary['ct'] = meta_data[summary['sample_name']]
+
     return summary
 
 
@@ -179,8 +226,11 @@ def write_qc_summary(summary):
     '''
     A function to write the QC data line to output in the order:
     * sample name
+    # % n bases
     * % bases covered
     * total mutations
+    * total snv
+    * total indel
     * total N mutations
     * total IUPAC mutations
     * mean sequence depth
@@ -188,33 +238,49 @@ def write_qc_summary(summary):
     * iVar QC pass
 
     Arguments:
-        * header: a list containing the column header
+        * summary:  dictionary containing the keys sample_name, pct_n_bases,
+                    pct_covered_bases, total_variants, total_snv, total_indel,
+                    total_n, total_iupac, mean_depth, median_depth, ct, qc_pass
 
     Return Value:
         None
     '''
     summary_line = '\t'.join([
         summary['sample_name'],
+        str(summary['pct_n_bases']),
         str(summary['pct_covered_bases']),
         str(summary['total_variants']),
+        str(summary['total_snv']),
+        str(summary['total_indel']),
         str(summary['total_n']),
         str(summary['total_iupac']),
         str(summary['mean']),
         str(summary['median']),
+        str(summary['ct']),
         str(summary['qc_pass'])])
     print(summary_line)
 
 
-def write_qc_summary_header(header=['sample_name', \
-                                    'pct_covered_bases',\
-                                    'total_variants', \
-                                    'total_n', \
-                                    'total_iupac', \
-                                    'mean_depth', \
-                                    'median_depth', \
+def write_qc_summary_header(header=['sample_name',
+                                    'pct_n_bases',
+                                    'pct_covered_bases',
+                                    'total_variants',
+                                    'total_snv',
+                                    'total_indel',
+                                    'total_n',
+                                    'total_iupac',
+                                    'mean_depth',
+                                    'median_depth',
+                                    'ct',
                                     'qc_pass']):
     '''
     Write the header for the QC summary data
+
+    Arguments:
+        * header: a list containing the column header
+
+    Return Value:
+        None
     '''
     print('\t'.join(header))
 
