e6a8062530b67f0ac2ac61446725ac52f8116df4
Richard de Borja <richard.deborja@gmail.com>
2020-05-25 10:55:57 -0400

    code cleanup for pylint and pep8 compliance

--- bin/collect_qc_summary.py
+++ bin/collect_qc_summary.py
@@ -8,9 +8,11 @@ import argparse
 import sys
 from ncov.parser.qc import collect_qc_summary_data, write_qc_summary_header
 
-parser = argparse.ArgumentParser(description="Tool for aggregating sample QC data")
-parser.add_argument('-p', '--path', \
-    help='directory to search for <sample>.summary.qc.tsv files')
+parser = argparse.ArgumentParser(description="Tool for aggregating sample QC \
+                                 data")
+parser.add_argument('-p', '--path',
+                    help='directory to search for <sample>.summary.qc.tsv \
+                         files')
 
 if len(sys.argv) == 1:
     parser.print_help(sys.stderr)
