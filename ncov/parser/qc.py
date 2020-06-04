3ab9aa99d5e06caa7260a6ddc5761df66426d135
Richard de Borja <richard.deborja@gmail.com>
2020-06-04 09:52:28 -0400

    added nanopore file support including vcf and calculating QC fields

--- ncov/parser/qc.py
+++ ncov/parser/qc.py
@@ -4,10 +4,12 @@ generated file contains a header line and a data line with pre-defined columns.
 '''
 
 import re
+import os
 import statistics
 import glob
 import csv
 from Bio import SeqIO
+import vcf
 
 
 def get_qc_data(file):
@@ -33,6 +35,96 @@ def get_qc_data(file):
             'pct_n_bases' : data[1],
             'pct_covered_bases' : data[2],
             'qc_pass' : data[6]}
+
+
+def get_qc_nanopore_data(fasta, reference):
+    '''
+    In the currently pipeline, the <sample>.qc.csv file is not generated for
+    nanopore runs.  We can generate the pct_n_bases and the sample_name and
+    leave the remaining fields as NA
+    '''
+    genome_length = 0
+    qc_data = {}
+    pct_n_bases = ''
+    try:
+        qc_data.update(count_iupac_in_fasta(fasta=fasta))
+        sample_name = re.sub('.consensus.fa', '', os.path.basename(fasta))
+        for record in SeqIO.parse(reference, 'fasta'):
+            genome_length = len(str(record.seq))
+        pct_n_bases = float(qc_data['total_n']) / genome_length * 100
+        pct_n_bases = str(round(pct_n_bases, 2))
+    except:
+        genome_length = 0
+        pct_n_bases = 'NA'
+    return {'sample_name': sample_name,
+            'pct_n_bases' : pct_n_bases,
+            'pct_covered_bases' : 'NA',
+            'qc_pass' : 'NA'}
+
+
+def get_total_variants_vcf(file, reference, start=1, mask_start=100,
+                           mask_end=50, indel=True):
+    '''
+    Get the total variants, total snv, total indels, total masked SNVs,
+    total masked indels and the genome length from the VCF file.
+    '''
+    counter = 0
+    counter_snv = 0
+    counter_indel = 0
+    counter_snv_masked = 0
+    counter_indel_masked = 0
+    genome_length = 0
+    counter_indel_triplet = 0
+
+    try:
+        for record in SeqIO.parse(reference, 'fasta'):
+            genome_length = len(str(record.seq))
+    except:
+        genome_length = 0
+
+    vcf_reader = vcf.Reader(filename=file)
+    for var in vcf_reader:
+        # keep a count of total variants
+        if indel:
+            counter += 1
+            if var.is_indel:
+                counter_indel += 1
+                if is_indel_triplet(str(var.ALT[0])):
+                    counter_indel_triplet += 1
+                if genome_length > 0:
+                    if is_base_masked(pos=int(var.POS),
+                                        end=genome_length,
+                                        mask_start=mask_start,
+                                        mask_end=mask_end):
+                        counter_indel_masked += 1                        
+            if var.is_snp:
+                counter_snv += 1
+                if is_base_masked(pos=int(var.POS),
+                                  end=genome_length,
+                                  mask_start=mask_start,
+                                  mask_end=mask_end):
+                    counter_snv_masked += 1
+        elif not indel:
+            if var.is_snp:
+                counter += 1
+                counter_snv += 1
+                if is_base_masked(pos=int(var.POS),
+                                  end=genome_length,
+                                  mask_start=mask_start,
+                                  mask_end=mask_end):
+                    counter_snv_masked += 1
+            if var.is_indel:
+                continue        
+        else:
+            continue
+    return {'total_variants' : counter,
+            'total_snv' : counter_snv,
+            'total_indel' : counter_indel,
+            'total_snv_masked' : counter_snv_masked,
+            'total_indel_masked' : counter_indel_masked,
+            'total_indel_triplet' : counter_indel_triplet,
+            'genome_length' : genome_length}
+
 
 
 def get_total_variants(file, reference, start=1, mask_start=100, mask_end=50,
@@ -83,7 +175,7 @@ def get_total_variants(file, reference, start=1, mask_start=100, mask_end=50,
                 if len(str(data[3])) > 1:
                     counter += 1
                     counter_indel += 1
-                    if is_indel_triplet(data[3]):
+                    if is_indel_triplet(str(data[3])):
                         counter_indel_triplet += 1
                     if genome_length > 0:
                         if is_base_masked(pos=int(data[1]),
@@ -120,13 +212,6 @@ def get_total_variants(file, reference, start=1, mask_start=100, mask_end=50,
             'total_indel_masked' : counter_indel_masked,
             'total_indel_triplet' : counter_indel_triplet,
             'genome_length' : genome_length}
-
-
-def get_variant_dictionary():
-    '''
-    Create a dictionary of variants per sample.
-    '''
-
 
 
 def import_ct_data(file, sample_id='sample', ct_id='ct'):
@@ -313,7 +398,7 @@ def get_coverage_stats(file):
 
 def create_qc_summary_line(var_file, qc_file, cov_file, meta_file=None,
                            indel=True, fasta=None, mask_start=100,
-                           mask_end=50, reference=None):
+                           mask_end=50, reference=None, instrument='illumina'):
     '''
     A function that aggregates the different QC data into a single sample
     dictionary entry.
@@ -345,6 +430,7 @@ def create_qc_summary_line(var_file, qc_file, cov_file, meta_file=None,
             * ct
     '''
     summary = {}
+    summary.update(count_iupac_in_fasta(fasta=fasta))
     summary.update(get_total_variants(file=var_file,
                                       indel=indel,
                                       reference=reference,
@@ -352,7 +438,6 @@ def create_qc_summary_line(var_file, qc_file, cov_file, meta_file=None,
                                       mask_end=mask_end))
     summary.update(get_qc_data(file=qc_file))
     summary.update(get_coverage_stats(file=cov_file))
-    summary.update(count_iupac_in_fasta(fasta=fasta))
 
     # import the ct data from the metadata file and add the ct value to the summary dictionary
     try:
@@ -456,3 +541,18 @@ def collect_qc_summary_data(path, pattern='.summary.qc.tsv'):
                     continue
                 data.append(line.rstrip())
     return data
+
+
+def check_instrument(instrument, valid_instruments=['illumina', 'nanopore']):
+    '''
+    Check whether the instrument provided is valid.
+
+    Arguments:
+        * instrument: name of instrument being used
+        * valid_instruments: a list of valid instruments
+
+    Return Value:
+        Returns a boolean determining whether the instrument is from a list of
+        valid instruments.
+    '''
+    return instrument in valid_instruments
