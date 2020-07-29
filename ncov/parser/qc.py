66227ee186820a21c83a4e02947fef2dca3b0d42
Richard de Borja <richard.deborja@gmail.com>
2020-07-29 09:40:13 -0400

    added nanopore functions for variants and qc data

--- ncov/parser/qc.py
+++ ncov/parser/qc.py
@@ -7,6 +7,7 @@ import re
 import statistics
 import glob
 import csv
+import vcf
 from Bio import SeqIO
 
 
@@ -33,6 +34,52 @@ def get_qc_data(file):
             'pct_n_bases' : pct_n_bases,
             'pct_covered_bases' : pct_covered_bases,
             'qc_pass' : qc_pass}
+
+
+def get_qc_nanopore_data(fasta, reference, consensus_suffix='.consensus.fa'):
+    '''
+    In the currently pipeline, the <sample>.qc.csv file is not generated for
+    nanopore runs.  We can generate the pct_n_bases and the sample_name and
+    leave the remaining fields as NA
+    '''
+    genome_length = 0
+    qc_data = {}
+    pct_n_bases = ''
+    try:
+        qc_data.update(count_iupac_in_fasta(fasta=fasta))
+        sample_name = re.sub('\.consensus\.fasta', '', os.path.basename(fasta))
+        for record in SeqIO.parse(reference, 'fasta'):
+            genome_length = len(str(record.seq))
+        pct_n_bases = float(qc_data['total_n']) / genome_length * 100
+        pct_n_bases = str(round(pct_n_bases, 2))
+    except:
+        genome_length = 0
+        pct_n_bases = 'NA'
+    return {'sample_name': sample_name,
+            'pct_n_bases' : pct_n_bases,
+            'pct_covered_bases' : 'NA',
+            'qc_pass' : 'NA'}
+
+
+def create_vcf_variant_id(var):
+    '''
+    Create a variant ID consisting of position, reference allele,
+    alternate allele as a concatenated string.  Note that the alternate
+    allele is provided as a list and needs to be set as a string
+    and merged.
+
+    Arguments:
+        * var: a VCF record object from pyvcf
+
+    Return Value:
+        Returns a string containing a concatenation of variant
+        object identifiers.
+    '''
+    var_alt = []
+    for _var in var.ALT:
+        var_alt.append(str(_var))
+    var_alt_str = ''.join(var_alt)
+    return '_'.join([str(var.POS), var.REF, var_alt_str])
 
 
 def get_total_variants(file, reference, mask_start=100, mask_end=50,
@@ -102,6 +149,74 @@ def get_total_variants(file, reference, mask_start=100, mask_end=50,
                 else:
                     continue
     file_p.close()
+    return {'total_variants' : counter_snv,
+            'total_indel' : counter_indel,
+            'total_snv_masked' : counter_snv_masked,
+            'total_indel_masked' : counter_indel_masked,
+            'total_indel_triplet' : counter_indel_triplet,
+            'genome_length' : genome_length}
+
+
+def get_total_variants_vcf(file, reference, start=1, mask_start=100,
+                           mask_end=50, indel=True):
+    '''
+    Get the total variants, total snv, total indels, total masked SNVs,
+    total masked indels and the genome length from the VCF file.
+    '''
+    counter = 0
+    counter_snv = 0
+    counter_indel = 0
+    counter_snv_masked = 0
+    counter_indel_masked = 0
+    genome_length = 0
+    counter_indel_triplet = 0
+    var_dict = {}
+    try:
+        for record in SeqIO.parse(reference, 'fasta'):
+            genome_length = len(str(record.seq))
+    except:
+        genome_length = 0
+
+    vcf_reader = vcf.Reader(filename=file)
+    for var in vcf_reader:
+        # create a unique variant identifier for de-duplication
+        var_id = create_vcf_variant_id(var=var)
+        if var_id in var_dict:
+            continue
+        else:
+            var_dict[var_id] = 1
+
+        # check if variant position is masked
+        base_masked = is_base_masked(pos=int(var.POS),
+                                     end=genome_length,
+                                     mask_start=mask_start,
+                                     mask_end=mask_end)
+        # keep a count of total variants
+        if indel:
+            counter += 1
+            if var.is_indel:
+                counter_indel += 1
+                if is_indel_triplet(re.sub('^.', '', str(var.ALT[0]))):
+                    counter_indel_triplet += 1
+                elif is_indel_triplet(re.sub('^.', '', str(var.REF))):
+                    counter_indel_triplet += 1
+                if genome_length > 0:
+                    if base_masked:
+                        counter_indel_masked += 1
+            if var.is_snp:
+                counter_snv += 1
+                if base_masked:
+                    counter_snv_masked += 1
+        elif not indel:
+            if var.is_snp:
+                counter += 1
+                counter_snv += 1
+                if base_masked:
+                    counter_snv_masked += 1
+            if var.is_indel:
+                continue
+        else:
+            continue
     return {'total_variants' : counter,
             'total_snv' : counter_snv,
             'total_indel' : counter_indel,
